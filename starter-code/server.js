a5733a405d05fc8dc904e290e8a3e97a32912618
David Nishikawa <triptype@gmail.com>
2017-09-05 14:53:01 -0700

    redo

--- starter-code/server.js
+++ starter-code/server.js
@@ -6,35 +6,29 @@
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
 // Be sure to install that and save it as a dependency after you create your package.json.
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
-// const bodyParser = require('body-parser').urlencoded({extended: true});
-// const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser').urlencoded({extended: true});
+const PORT = process.env.PORT || 3000;
 
-// // TODO: Include all of the static resources as an argument to app.use()
+// TODO: Include all of the static resources as an argument to app.use()
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
-// // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
 
 
-// app.post('/articles', bodyParser, function(request, response) {
-//   // REVIEW: This route will receive a new article from the form page, new.html,
-//   // and log that form data to the console. We will wire this up soon to actually
-//   // write a record to our persistence layer!
-//   console.log(request.body);
-//   response.send('Record posted to server!!');
-// })
+app.post('/articles', bodyParser, function(request, response) {
+  // REVIEW: This route will receive a new article from the form page, new.html,
+  // and log that form data to the console. We will wire this up soon to actually
+  // write a record to our persistence layer!
+  console.log(request.body);
+  response.send('Record posted to server!!');
+})
 
-// app.listen(PORT, function() {
-//   // TODO: Log to the console a message that lets you know which port your server has started on
-// });
+app.listen(PORT, function() {
+  // TODO: Log to the console a message that lets you know which port your server has started on
+});
 
-let i = 5;
-var b = 7;
-function addB () {
-  
-  console.log( i + 'sss' + b);
-}
-console.log(i);
+
